b1909458e8ae93ada3eb54886d36c7168afc8ff6
frontjsw <89008053+frontjsw@users.noreply.github.com>
2022-01-19 23:51:11 +0900

    Create Details,Write page

---
+++ front/src/pages/Details.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  width: 80%;
+  margin: 10px auto;
+  padding: 10px 0;
+  text-align: center;
+  border: 1px solid #ccc;
+`;
+
+const Content = styled.div`
+  border: 1px solid #ccc;
+  width: 80%;
+  margin: 0 auto;
+  height: 100px;
+  padding: 10px;
+`;
+
+const Detail = () => {
+  return (
+    <Wrapper>
+      <Title>글 제목</Title>
+      <Content>
+        임시 텍스트입니다. 임시 텍스트입니다. 임시 텍스트입니다. 임시
+        텍스트입니다.
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Detail;
