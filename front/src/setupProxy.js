d31a9f4f501922ab2ec5e31856e5d6fa9b312699
GongZone <dnjsqhwo@gmail.com>
2022-01-21 18:16:43 +0900

    테스트

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    createProxyMiddleware("/board", {
       target: "http://localhost:8082",
       changeOrigin: true,
     })
