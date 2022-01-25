2f6c2a57d78a68047577d7ed93503b0d2f82bd9d
frontjsw <89008053+frontjsw@users.noreply.github.com>
2022-01-25 19:28:39 +0900

    로그인,회원가입 Form 구현

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import { useForm } from "react-hook-form";
 const Wrapper = styled.div`
   margin: 0 auto;
   width: 100%;
@@ -29,7 +29,6 @@ const Form = styled.form`
     display: block;
     width: 100%;
     height: 35px;
-    margin-bottom: 20px;
     border: 1px solid #dfe6e9;
     background-color: #fafafa;
     padding-left: 10px;
@@ -37,7 +36,7 @@ const Form = styled.form`
   button {
     width: 30%;
     max-width: 100px;
-    margin: 0 auto;
+    margin: 5px auto 0 auto;
     display: block;
     height: 40px;
     border-radius: 15px;
@@ -53,8 +52,17 @@ const Form = styled.form`
       background-color: #fff;
     }
   }
+  .errorMessage {
+    font-size: 10px;
+    color: red;
+    line-height: 20px;
+  }
 `;
 
+const InputBox = styled.div`
+  height: 65px;
+  padding: 15px 0;
+`;
 const Register = styled.div`
   text-align: center;
   font-size: 12px;
@@ -65,18 +73,62 @@ const Register = styled.div`
 `;
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onValid = (data) => {
+    // 기본으로 data 가져오기
+    console.log(data);
+  };
   return (
     <Wrapper>
       <Header>로그인</Header>
-      <Form>
-        <input type="text" placeholder="사용자 이름 또는 이메일" />
-        <input type="text" placeholder="비밀번호" />
+      <Form onSubmit={handleSubmit(onValid)}>
+        <InputBox>
+          <input
+            {...register("id", {
+              required: "true",
+              minLength: {
+                value: 6,
+                message: "이름은 6자 이상으로 입력하여 주시길 바랍니다.",
+              },
+            })}
+            type="text"
+            placeholder="사용자 이름 또는 이메일"
+          />
+          <div className="errorMessage">
+            {errors.id?.type === "required" &&
+              "이름을 필수 조건으로 입력하시길 바랍니다."}
+            {errors.id?.type === "minLength" && errors.id.message}
+          </div>
+        </InputBox>
+        <InputBox>
+          <input
+            {...register("password", {
+              required: "true",
+              minLength: {
+                value: 6,
+                message: "비밀번호는 6자 이상으로 입력하여 주시길 바랍니다.",
+              },
+            })}
+            type="text"
+            placeholder="비밀번호"
+          />
+          <div className="errorMessage">
+            {errors.password?.type === "required" &&
+              "비밀번호는 필수 조건으로 입력하시길 바랍니다."}
+            {errors.password?.type === "minLength" && errors.password.message}
+          </div>
+        </InputBox>
         <button>Login</button>
       </Form>
       <Register>
-        계정이 없으신가요?{" "}
+        계정이 없으신가요?
         <Link to="/register">
-          <span>회원가입</span>
+          <span> 회원가입</span>
         </Link>
       </Register>
     </Wrapper>
